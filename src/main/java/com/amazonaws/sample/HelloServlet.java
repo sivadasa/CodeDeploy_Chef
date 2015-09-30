45bd6e791bb337369974fda287268a855de684ef
sivadasa <sivadasa@amazon.com>
2015-09-30 16:47:35 -0700

    Update HelloServlet.java

--- src/main/java/com/amazonaws/sample/HelloServlet.java
+++ src/main/java/com/amazonaws/sample/HelloServlet.java
@@ -15,6 +15,6 @@ public class HelloServlet extends HttpServlet {
         throws ServletException, IOException
     {
         PrintWriter writer = response.getWriter();
-        writer.print("<h1>Hello World 1</h1>");
+        writer.print("<h1>Hello World 2</h1>");
     }
 }
